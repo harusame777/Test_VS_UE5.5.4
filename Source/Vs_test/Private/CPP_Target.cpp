cae0d090e2a8fd354dc83b07c8d9d0878357c835
takeda haruse <haruse010655@gmail.com>
2025-09-02 10:32:14 +0900

    ターゲットを作成

---
+++ Source/Vs_test/Private/CPP_Target.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_Target.h"
+
+// Sets default values
+ACPP_Target::ACPP_Target()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_Target::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACPP_Target::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
